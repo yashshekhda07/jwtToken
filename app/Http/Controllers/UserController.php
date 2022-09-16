1c3af7d8fa539fa250fe728cc7c1ac4e290cfeff
Yash Shekhda <106952814+yash-shekhda@users.noreply.github.com>
2022-09-16 23:51:45 +0530

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use App\Models\User;
+// use illuminate\Support\Facades\Hash;
+use Validator;
+use Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $validate = Validator::make($request->all(),[
+            'name'=>'required|min:2|max:11',
+            'email'=>'required|email',
+            'password'=>'required|confirmed',
+        ]);
+
+        if($validate->fails())
+        {
+            return response()->json($validate->errors(),400);
+        }
+
+        $user = User::create([  
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+
+        return response()->json(['msg'=>'User Created Successsfully','user'=>$user]);
+    }
+
+    public function login(Request $request){        
+        $validate = Validator::make($request->all(),[
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+
+        if($validate->fails())
+        {
+            return response()->json($validate->errors(),400);
+        }
+
+        if(!$token = auth()->attempt($validate->validated()))
+        {
+            return response()->json(['msg'=>'Unauthorized']);
+        }
+
+        return $this->responseWithToken($token);
+    }
+
+    protected function responseWithToken($token){
+        return response()->json([
+            'access_token'=>$token,
+            'type'=>'bearer',
+            'expires_in'=>auth()->factory()->getTTL()*60,
+
+        ]);
+    }
+
+    public function profile()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function refresh()
+    {
+        return $this->responseWithToken(auth()->refresh());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['msg'=>'User Logged Out Successfully']);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['middleware'=>'api'],function($routes){
+    Route::post('register',[UserController::class,'register']);
+    Route::post('login',[UserController::class,'login']);
+    Route::post('profile',[UserController::class,'profile']);
+    Route::post('refresh',[UserController::class,'refresh']);
+    Route::post('logout',[UserController::class,'logout']);
+});
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
